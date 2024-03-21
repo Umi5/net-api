9216b30e87ab6e55ef0b8ea69cea22813dccf07d
Manuel Busquier <manubusqui@gmail.com>
2024-03-21 19:03:02 +0100

    Add test folder

--- API.Tests/UnitTest1.cs
+++ API.Tests/UnitTest1.cs
@@ -1,10 +1,17 @@
+using AutoMapper;
+using Data;
+using Services.CharacterServices;
+
 namespace API.Tests;
 
 public class UnitTest1
 {
+    private readonly IMapper _mapper;
+    private readonly DataContext _context;
     [Fact]
-    public void Test1()
+    public void GetAllCharacter_Returns_ServiceResponse()
     {
-
+        
     }
+
 }
