726c87c69601ea2ff30f18a54ca8807ca12f4bda
Manuel <manubusqui@gmail.com>
2024-03-21 10:45:59 +0100

    new migration with damage atribute for weapon

--- Models/Weapon.cs
+++ Models/Weapon.cs
@@ -9,6 +9,7 @@ namespace Models
     {
         public Guid Id { get; set; }
         public string? Name { get; set; }
+        public int Damage { get; set; }
         public Character? Character { get; set; }
         public Guid CharacterId { get; set; }
     }
